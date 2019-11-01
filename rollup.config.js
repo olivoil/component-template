4c7d94397b89d0c230fefd7e9e04ceb275e2f0ba
Olivier Melcher <olivier.melcher@gmail.com>
2019-11-01 14:17:45 -0500

    feat: add css, ssr, and custom element options to rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,17 +1,36 @@
 import svelte from "rollup-plugin-svelte";
 import resolve from "rollup-plugin-node-resolve";
+import livereload from "rollup-plugin-livereload";
 import pkg from "./package.json";
+
+require('dotenv').config();
+const production = !process.env.ROLLUP_WATCH;
+const ssr = !!process.env.SSR;
+const customElement = !!process.env.CUSTOM_ELEMENT;
+const css = !!process.env.CSS;
 
 const name = pkg.name
   .replace(/^(@\S+\/)?(svelte-)?(\S+)/, "$3")
   .replace(/^\w/, m => m.toUpperCase())
   .replace(/-\w/g, m => m[1].toUpperCase());
 
-export default {
+const svelteOptions = { customElement };
+if (css) svelteOptions.css = (css) => css.write(production ? pkg.style : "public/index.css");
+if (ssr) svelteOptions.generate = 'ssr';
+
+const config = {
   input: "src/index.svelte",
-  output: [
-    { file: pkg.module, format: "es" },
-    { file: pkg.main, format: "umd", name },
+  output: production
+    ? [
+        { file: pkg.module, format: "es" },
+        { file: pkg.main, format: "umd", name },
+      ]
+    : { name, format: "umd", file: "public/index.js" },
+  plugins: [
+    svelte(svelteOptions),
+    resolve(),
+    !production && livereload("public"),
   ],
-  plugins: [svelte(), resolve()],
 };
+
+export default config;
